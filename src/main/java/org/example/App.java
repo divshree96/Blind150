29efeac23dc86f4e395c9b5927ba05fffff36825
Divya Shree <shree.d@northeastern.edu>
2023-05-25 20:43:25 -0400

    Generate Parentheses Problem 15

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
+        GenerateParentheses15 generateParentheses15 = new GenerateParentheses15();
+        System.out.println(generateParentheses15.generateParenthesis(3));
+
         System.out.println( "Hello World!" );
     }
 }
