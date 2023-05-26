b3c062faa6f79376995c1f5330577c1636135b33
Divya Shree <shree.d@northeastern.edu>
2023-05-25 20:14:10 -0400

    Evaluate Reverse Polish Notation Problem 14

---
+++ src/main/java/org/example/ReversePolishNotation14.java
@@ -0,0 +1,68 @@
+package org.example;
+
+import java.util.Stack;
+
+/**
+ * You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+ *
+ * Evaluate the expression. Return an integer that represents the value of the expression.
+ *
+ * Note that:
+ * The valid operators are '+', '-', '*', and '/'.
+ * Each operand may be an integer or another expression.
+ * The division between two integers always truncates toward zero.
+ * There will not be any division by zero.
+ * The input represents a valid arithmetic expression in a reverse polish notation.
+ * The answer and all the intermediate calculations can be represented in a 32-bit integer.
+ *
+ * Input: tokens = ["2","1","+","3","*"]
+ * Output: 9
+ * Explanation: ((2 + 1) * 3) = 9
+ *
+ * Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+ * Output: 22
+ * Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+ * = ((10 * (6 / (12 * -11))) + 17) + 5
+ * = ((10 * (6 / -132)) + 17) + 5
+ * = ((10 * 0) + 17) + 5
+ * = (0 + 17) + 5
+ * = 17 + 5
+ * = 22
+ */
+public class ReversePolishNotation14 {
+    public static void main(String[] args){
+        System.out.println(evalRPN(new String[]{"10","6","9","3","+","-11","*","/","*","17","+","5","+"}));
+    }
+
+    public static int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        for(String s : tokens){
+            if(s.equals("+") || s.equals("-") ||
+                    s.equals("*") || s.equals("/")) {
+                int a = stack.pop();
+                int b = stack.pop();
+
+                switch(s){
+                    case "+":
+                        stack.push(a+b);
+                        break;
+                    case "-":
+                        stack.push(b-a);
+                        break;
+                    case "*":
+                        stack.push(a*b);
+                        break;
+                    case "/":
+                        stack.push(b/a);
+                        break;
+                }
+            }
+            else {
+                stack.push(Integer.parseInt(s));
+            }
+        }
+        return stack.pop();
+    }
+
+}
